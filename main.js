9b280ab823f7ad06c5dc5e7e0b9e0fe4c7086555
Michal Plíšek <hello.plisek@gmail.com>
2026-04-19 15:55:08 +0200

    Update APP_VERSION to 20250420

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ window.allProducts = allProducts;
 
 //Nová verze aplikace APP_VERSION VERZE APP VERSION
 
-const APP_VERSION = '20250419';
+const APP_VERSION = '20250420';
 const storedVersion = localStorage.getItem('app_version');
 if (storedVersion !== APP_VERSION) {
     localStorage.clear();
